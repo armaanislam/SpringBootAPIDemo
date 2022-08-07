594384fcfeec20eaafbc72de8c5eb9c2e999ba15
MD. Arman Islam <armaanislam84@gmail.com>
2022-08-07 20:01:58 +0600

    Spring Profiles and Run Command

--- src/main/java/com/springdemo/springbootapi/SpringbootapiApplication.java
+++ src/main/java/com/springdemo/springbootapi/SpringbootapiApplication.java
@@ -8,6 +8,12 @@ public class SpringbootapiApplication {
 	// Postman Client <-> Controller <-> Service <-> DAO <-> DB
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootapiApplication.class, args);
+//		Run Commands:
+//		mvnspring-boot:run
+//		mvn clean install
+//		cd target
+//		dir
+//		java -jar springbootapi-1.0.0.jar --spring.profiles.active=prod
 	}
 
 }
