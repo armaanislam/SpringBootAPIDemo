717bd1240611f63542006ae4ccd44d130bc8d34b
MD. Arman Islam <armaanislam84@gmail.com>
2022-08-07 16:11:48 +0600

    Adding Config in Properties File

--- src/main/java/com/springdemo/springbootapi/controller/HelloController.java
+++ src/main/java/com/springdemo/springbootapi/controller/HelloController.java
@@ -1,17 +1,20 @@
 package com.springdemo.springbootapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 // @Controller // To add this class to the container; works same as @Component, but we use @Controller in case of a controller
 @RestController // In case of APIs; it is also a component which returns a reponse body
 public class HelloController {
 
+    @Value("${welcome.message}") // Adding the config form properties file
+    private String welcomeMessage;
+
     // @RequestMapping(value = "/", method = RequestMethod.GET) // (Endpoint, HTTP Method)
     @GetMapping("/") // Same as above
     public String helloWorld(){
-        return "Hello, World!";
+//        return "Hello, World!";
+        return welcomeMessage;
     }
 }
